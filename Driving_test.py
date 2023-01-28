211e1620b443b55acbb7dbff7c96899ee729f000
samir_patni <47419003+samirpatni9090@users.noreply.github.com>
2023-01-28 15:47:54 +0530

    Add files via upload

---
+++ Driving_test.py
@@ -0,0 +1,10 @@
+s1 = int(input("PLease Enter your age :"))
+
+if s1 == 18:
+    print(" PLease Wait For One Year ")
+elif s1 > 18:
+    print('You Can Drive ')
+elif s1 < 18:
+    print("Your Can't Drive")
+else :
+    print("PLease Check Input")
